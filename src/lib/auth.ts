eaf830e1975cd9bba371a8961eb70bb7d27a2343
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2024-03-05 13:45:09 +0700

    update fix error4

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,7 +37,7 @@ export const authOptions: NextAuthOptions = {
     maxAge: 30 * 60,
   },
   debug: true,
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
